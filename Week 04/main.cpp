3470785b216583ad18d65ffce6f0fa8fd29e3660
tapeau <john_lorenzo_tapia@dlsu.edu.ph>
2024-10-03 20:50:42 +0800

    Implement GPU summary table - Part 2

--- Week 04/main.cpp
+++ Week 04/main.cpp
@@ -32,7 +32,6 @@ int main()
     std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     std::cout << std::ctime(&now);
 
-    // TODO: Print GPU summary table
     // GPU specifications represented as array of strongs
     std::string gpuSpecs[] = { "551.86", "551.86", "12.4", "0", "28%", "37C", "NVIDIA GeForce GTX 1080", "WDDM", "P8", "11W", "180W", "00000000:26:00.0", "On", "701MiB", "8192MiB", "0%", "N/A", "Default", "N/A" };
 
@@ -44,7 +43,18 @@ int main()
     std::cout << "|\n|-----------------------------------------+------------------------+----------------------+\n| GPU  Name                     TCC/WDDM  | Bus-Id          Disp.A | Volatile Uncorr. ECC |\n| Fan  Temp   Perf          Pwr:Usage/Cap |           Memory-Usage | GPU-Util  Compute M. |\n|                                         |                        |               MIG M. |\n|=========================================+========================+======================|\n| ";
 
     // Print GPU summary table values
-
+    printConsole(gpuSpecs[3], 3, 'r'); setCursorPos(7, getConsoleBuffer().dwCursorPosition.Y);
+    printConsole(gpuSpecs[6], 23, 'l'); setCursorPos(31, getConsoleBuffer().dwCursorPosition.Y);
+    printConsole(gpuSpecs[7], 9, 'r'); std::cout << "  | "; printConsole(gpuSpecs[11], 18, 'r'); std::cout << " ";
+    printConsole(gpuSpecs[12], 3, 'r'); std::cout << " | "; printConsole(gpuSpecs[16], 20, 'r'); std::cout << " |\n| ";
+    printConsole(gpuSpecs[4], 4, 'l'); std::cout << " "; printConsole(gpuSpecs[5], 4, 'r');
+    setCursorPos(15, getConsoleBuffer().dwCursorPosition.Y); printConsole(gpuSpecs[8], 2, 'r'); setCursorPos(28, getConsoleBuffer().dwCursorPosition.Y);
+    printConsole(gpuSpecs[9], 5, 'r'); std::cout << " / "; printConsole(gpuSpecs[10], 5, 'r'); std::cout << " |  ";
+    printConsole(gpuSpecs[13], 9, 'r'); std::cout << " / "; printConsole(gpuSpecs[14], 9, 'r'); std::cout << " |";
+    setCursorPos(72, getConsoleBuffer().dwCursorPosition.Y); printConsole(gpuSpecs[15], 4, 'r');
+    setCursorPos(79, getConsoleBuffer().dwCursorPosition.Y); printConsole(gpuSpecs[17], 10, 'r');
+    std::cout << " |\n|";
+    setCursorPos(42, getConsoleBuffer().dwCursorPosition.Y); std::cout << "|"; setCursorPos(67, getConsoleBuffer().dwCursorPosition.Y); std::cout << "| "; printConsole(gpuSpecs[16], 20, 'r'); std::cout << " |\n+-----------------------------------------+------------------------+----------------------+\n";
     
     // TODO: Print process screen
 
